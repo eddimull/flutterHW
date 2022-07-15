499ec4ff8f82afe42dffa1eba97c5bad81c6e085
eddimull <eddimull@gmail.com>
2022-07-15 13:43:26 -0500

    centered the login input

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -10,16 +10,46 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Login'),
+          ),
+          body: Container(
+              color: Theme.of(context).primaryColorDark,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Card(
+                        elevation: 8,
+                        margin: EdgeInsets.only(left: 16, right: 16, top: 16),
+                        child: Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Column(children: <Widget>[
+                            TextField(
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(labelText: 'Email'),
+                            ),
+                            TextField(
+                              keyboardType: TextInputType.visiblePassword,
+                              decoration:
+                                  InputDecoration(labelText: 'Password'),
+                            ),
+                            Container(
+                                margin: const EdgeInsets.only(top: 10),
+                                child: ElevatedButton(
+                                  onPressed: () => print('login clicked'),
+                                  child: Text('Login'),
+                                  style: ElevatedButton.styleFrom(
+                                      primary: Colors.teal,
+                                      onPrimary: Colors.white,
+                                      onSurface: Colors.grey,
+                                      minimumSize: Size(double.infinity, 36)),
+                                )),
+                          ]),
+                        ))
+                  ]))),
     );
   }
 }
