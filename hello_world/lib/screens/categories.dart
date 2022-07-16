7d2288e2ae63751c306e8be4c8a9885860dc4c09
eddimull <eddimull@gmail.com>
2022-07-16 12:38:18 -0500

    can make http requests!

--- hello_world/lib/screens/categories.dart
+++ hello_world/lib/screens/categories.dart
@@ -1,4 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Category {
+  int id;
+  String name;
+
+  Category({required this.id, required this.name});
+
+  factory Category.fromJson(Map<String, dynamic> json) {
+    return Category(
+      id: json['id'],
+      name: json['name'],
+    );
+  }
+}
 
 class Categories extends StatefulWidget {
   const Categories({Key? key}) : super(key: key);
@@ -8,7 +25,23 @@ class Categories extends StatefulWidget {
 }
 
 class CategoriesState extends State<Categories> {
-  int clicked = 0;
+  late Future<List<Category>> futureCategories;
+
+  Future<List<Category>> fetchCategories() async {
+    http.Response response =
+        await http.get(Uri.parse('http://localhost/api/categories'));
+    // print(response.body);
+    List categories = jsonDecode(response.body);
+
+    return categories.map((category) => Category.fromJson(category)).toList();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureCategories = fetchCategories();
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<String> categories = <String>[
@@ -19,20 +52,29 @@ class CategoriesState extends State<Categories> {
 
     return Scaffold(
         appBar: AppBar(
-          title: Text('Categories $clicked'),
+          title: Text('Categories'),
         ),
         body: Container(
             color: Theme.of(context).primaryColorDark,
             child: Center(
-              child: ListView.builder(
-                  itemCount: categories.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ListTile(
-                        title: Text(categories[index],
-                            style: const TextStyle(color: Colors.white)),
-                        onTap: () =>
-                            setState(() => {clicked++, print('clicked')}));
-                  }),
-            )));
+                child: FutureBuilder<List<Category>>(
+                    future: futureCategories,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return ListView.builder(
+                          itemCount: snapshot.data!.length,
+                          itemBuilder: (context, index) {
+                            Category category = snapshot.data![index];
+                            return ListTile(
+                              title: Text(category.name),
+                            );
+                          },
+                        );
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
+
+                      return const CircularProgressIndicator();
+                    }))));
   }
 }
