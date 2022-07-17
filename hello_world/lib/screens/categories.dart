777a74cdbcc68dc37c217618719b535211913cd3
eddimull <eddimull@gmail.com>
2022-07-17 15:43:13 -0500

    can make http put request to update the category name

--- hello_world/lib/screens/categories.dart
+++ hello_world/lib/screens/categories.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -26,6 +27,9 @@ class Categories extends StatefulWidget {
 
 class CategoriesState extends State<Categories> {
   late Future<List<Category>> futureCategories;
+  final _formKey = GlobalKey<FormState>();
+  late Category selectedCategory;
+  final categoryNameController = TextEditingController();
 
   Future<List<Category>> fetchCategories() async {
     http.Response response =
@@ -34,6 +38,25 @@ class CategoriesState extends State<Categories> {
     List categories = jsonDecode(response.body);
 
     return categories.map((category) => Category.fromJson(category)).toList();
+  }
+
+  Future saveCategory() async {
+    final form = _formKey.currentState;
+
+    if (!form!.validate()) {
+      return;
+    } else {
+      String uri = 'http://localhost/api/categories/${selectedCategory.id}';
+      await http.put(Uri.parse(uri),
+          headers: {
+            HttpHeaders.contentTypeHeader: 'application/json',
+            HttpHeaders.acceptHeader: 'application/json'
+          },
+          body: jsonEncode({'name': categoryNameController.text}));
+
+      // ignore: use_build_context_synchronously
+      Navigator.pop(context);
+    }
   }
 
   @override
@@ -52,7 +75,7 @@ class CategoriesState extends State<Categories> {
 
     return Scaffold(
         appBar: AppBar(
-          title: Text('Categories'),
+          title: const Text('Categories'),
         ),
         body: Container(
             color: Theme.of(context).primaryColorDark,
@@ -67,6 +90,45 @@ class CategoriesState extends State<Categories> {
                             Category category = snapshot.data![index];
                             return ListTile(
                               title: Text(category.name),
+                              trailing: IconButton(
+                                  icon: const Icon(Icons.edit),
+                                  onPressed: () {
+                                    selectedCategory = category;
+                                    categoryNameController.text = category.name;
+                                    showModalBottomSheet(
+                                        context: context,
+                                        builder: (context) {
+                                          return Form(
+                                              key: _formKey,
+                                              child: Container(
+                                                padding:
+                                                    const EdgeInsets.all(20),
+                                                child:
+                                                    Column(children: <Widget>[
+                                                  TextFormField(
+                                                      validator: (value) {
+                                                        if (value!.isEmpty) {
+                                                          return 'Enter category name';
+                                                        }
+                                                        return null;
+                                                      },
+                                                      controller:
+                                                          categoryNameController,
+                                                      decoration:
+                                                          const InputDecoration(
+                                                        border:
+                                                            OutlineInputBorder(),
+                                                        labelText:
+                                                            'Category Name',
+                                                      )),
+                                                  ElevatedButton(
+                                                      onPressed: () =>
+                                                          {saveCategory()},
+                                                      child: const Text('Save'))
+                                                ]),
+                                              ));
+                                        });
+                                  }),
                             );
                           },
                         );
